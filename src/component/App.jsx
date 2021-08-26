7efa316344c6010cbf26a8ae083d6e520b9fa4f7
yoan parede <parede.yoan.amcs@gmail.com>
2021-08-26 12:03:16 +0200

    Continuous on seo

--- src/component/App.jsx
+++ src/component/App.jsx
@@ -36,7 +36,6 @@ const App = () => {
   const [footOpen, setFootOpen] = useState(false);
   const [isLoaded, setIsLoaded] = useState(false);
   
-  TestScroll(xMax);
   //buildSitemap('./Router.jsx', '../../sitemap.xml', 'https://magin-code.fr');
   useEffect(()=>{
     let userIsConnected = sessionStorage.getItem('user_co');
@@ -57,33 +56,30 @@ const App = () => {
             document.getElementById('footer-principal').style.display='none';
             window.onscroll = ()=>{
               try{
-                  console.log(height)
-                  console.log(document.documentElement.scrollTop)
-                  if(largeur>850){
-                      if(document.documentElement.scrollTop > 200){
-                          document.getElementById('navDynamique').style.position = "fixed";
-                          document.getElementById('navDynamique').style.top = 0;
-                          document.getElementById('navDynamique').style.padding = "10px 0";
-          
-                      }else{
-                          document.getElementById('navDynamique').style.position = "relative";
-                          document.getElementById('navDynamique').style.top = 'initial';
-                          document.getElementById('navDynamique').style.padding = "0 0 20px";
-                      }
-                      if(document.documentElement.scrollTop > height){
-                          document.getElementById('footer-principal').style.display='block';
-                          
-                      }else{
-                          document.getElementById('footer-principal').style.display='none';
-              
-                      }
-                  }else{
-                      document.getElementById('footer-principal').style.display='block';
-
-                  }
-                  
-              }catch{
+                console.log(height)
+                console.log(document.documentElement.scrollTop)
+                if(largeur>850){
+                    if(document.documentElement.scrollTop > 200){
+                        document.getElementById('navDynamique').style.position = "fixed";
+                        document.getElementById('navDynamique').style.top = 0;
+                        document.getElementById('navDynamique').style.padding = "10px 0";
+        
+                    }else{
+                        document.getElementById('navDynamique').style.position = "relative";
+                        document.getElementById('navDynamique').style.top = 'initial';
+                        document.getElementById('navDynamique').style.padding = "0 0 20px";
+                    }
+                    if(document.documentElement.scrollTop > height){
+                        document.getElementById('footer-principal').style.display='block';
+                        
+                    }else{
+                        document.getElementById('footer-principal').style.display='none';
+                    }
+                }else{
+                    document.getElementById('footer-principal').style.display='block';
               }
+            }catch{
+            }
       }
     }
     }
