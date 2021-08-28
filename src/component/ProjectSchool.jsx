dc9aaf1aac2cbd30ce02846301cacb9e6947e77d
yoan parede <parede.yoan.amcs@gmail.com>
2021-08-28 14:41:21 +0200

    version 1 98% finish : rest :  PAGE ABOUT AND SEO & NEED PLAN FOR V1.1.X

--- src/component/ProjectSchool.jsx
+++ src/component/ProjectSchool.jsx
@@ -1,5 +1,11 @@
+import html5 from '../assets/images/static/p2.PNG';
+import p3 from '../assets/images/static/p3.PNG';
+import p4 from '../assets/images/static/p4.PNG';
+import p5 from '../assets/images/static/p5.PNG';
+import p6 from '../assets/images/static/p6.PNG';
+import p7 from '../assets/images/static/p7.PNG';
+import '../assets/scss/ProjectSchool.scss';
 const ProjectSchool = () => {
-
     return (
         <div className="page-projects-etude">
             <h4 className='mes-projets'><span className='little-text'>MES </span>PROJETS D'ÉTUDE</h4>
@@ -11,8 +17,7 @@ const ProjectSchool = () => {
                                 <header  className='header-projet'>
                                     <h3 >CV en HTML5 et CSS/3</h3>
                                 </header>
-                            
-                                    <img  className='img-projets' alt="Image du projet html5 css3" src=''/>
+                                <img  className='img-projets need-box-shadow' alt="Image du projet html5 css3" src={html5}/>
                                 <p className="desc-project" >Projet réaliser en HTML5 / CSS 3, début d'une grande aventure de merveilleux souvenir et naissance d'une passion</p>
                                 <footer  className="footer-projet">
                                     <p className="lien-projet-p" >Crée votre cv en HTML5 / CSS3</p> 
@@ -31,8 +36,7 @@ const ProjectSchool = () => {
                                 <header  className='header-projet'>
                                     <h3 >Site web animé</h3>
                                 </header>
-                            
-                                    <img  className='img-projets' alt="Image du projet animation avec css 3" src=''/>
+                                <img  className='img-projets need-box-shadow' alt="Image du projet animation avec css 3" src={p3}/>
                                 <p className="desc-project" >Projet réaliser en HTML5 / CSS 3 & SASS en préprocesseur, 
                                     avec ce projeyt j'ai gagné en puissance, et en experience sur les animations.
                                 </p>
@@ -53,8 +57,7 @@ const ProjectSchool = () => {
                                 <header  className='header-projet'>
                                     <h3 >Référencement & SEO</h3>
                                 </header>
-                            
-                                    <img  className='img-projets' alt="Image du projet html5 css3" src=''/>
+                                <img  className='img-projets need-box-shadow' alt="Image du projet SEO & Referencement" src={p4}/>
                                 <p className="desc-project" >Dans ce projet j'ai mis en pratique le SEO & le référencement, en optimisant un site web déjà existant 
                                     changement taille sources(images etc.), optimisation du code(la syntaxe, la sémantique) et adaptation en responsive design.
                                 </p>
@@ -76,7 +79,7 @@ const ProjectSchool = () => {
                                     <h3 >Site e-comerce</h3>
                                 </header>
                             
-                                    <img  className='img-projets' alt="Image du projet html5 css3" src=''/>
+                                    <img  className='img-projets need-box-shadow' alt="Image du projet création site e-comerce" src={p5}/>
                                 <p className="desc-project" >Projet réaliser en HTML5 / CSS 3 & JavaScript, projet super important premier script en JavaScript, se projet
                                  se concentre sur toute la partie frontend de la création d'un site e-comerce</p>
                                 <footer  className="footer-projet">
@@ -97,7 +100,7 @@ const ProjectSchool = () => {
                                     <h3 >Api avis gastronomique</h3>
                                 </header>
                             
-                                    <img  className='img-projets' alt="Image du projet html5 css3" src=''/>
+                                    <img  className='img-projets need-box-shadow' alt="Image du projet construire une api sécuriser" src={p6}/>
                                 <p className="desc-project" >Projet réaliser avec NodeJs, le but étant de créé une API pour pour un site répertoriant des sauces l'API est CRUD</p>
                                 <footer  className="footer-projet">
                                     <p className="lien-projet-p" >Crée une api d'avis gastronoique</p> 
@@ -117,7 +120,7 @@ const ProjectSchool = () => {
                                     <h3 >Reseau social d'entreprise</h3>
                                 </header>
                             
-                                    <img  className='img-projets' alt="Image du projet html5 css3" src=''/>
+                                    <img  className='img-projets' alt="Image du projet reseau social d'entreprise" src={p7}/>
                                 <p className="desc-project" >Ce projet ma fait d'écouvrir la joie de crée un site avec la vision d'un fullstack création integrale d'un reseau social
                                     autant côté backend que frontend.
                                     utilisation de NodeJs / reactJs et certaines de leur dépendences (exemple : express, json web token, react-dom-router, etc.).
