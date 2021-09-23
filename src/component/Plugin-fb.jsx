afd6d6c31b26ab6c1151da326935244a55dac289
yoan parede <parede.yoan.amcs@gmail.com>
2021-09-23 11:50:54 +0200

    Add fb messagery

---
+++ src/component/Plugin-fb.jsx
@@ -0,0 +1,13 @@
+export const PluginFb=()=>{
+    var tt=`
+    '
+    return({
+        // <!-- Messenger Plugin de discussion Code -->
+    
+    })`
+    return(
+        <>
+            
+        </>
+    )
+}
